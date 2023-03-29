58eaefe318a27e7975424e78a60911815cfe3838
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-03-29 09:48:04 +0200

    files structure

---
+++ sql/online_shop.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 29 mars 2023 à 07:45
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `online_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis_client`
+--
+
+DROP TABLE IF EXISTS `avis_client`;
+CREATE TABLE IF NOT EXISTS `avis_client` (
+  `id_avis` int(11) NOT NULL AUTO_INCREMENT,
+  `id_produit` int(11) NOT NULL,
+  `id_client` int(11) NOT NULL,
+  `titre_avis` text NOT NULL,
+  `commentaire_avis` text NOT NULL,
+  `note_avis` int(11) NOT NULL,
+  PRIMARY KEY (`id_avis`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id_categories` int(11) NOT NULL AUTO_INCREMENT,
+  `name_categories` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_categories`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `id_client` int(11) NOT NULL AUTO_INCREMENT,
+  `prenom_client` varchar(255) NOT NULL,
+  `nom_client` varchar(255) NOT NULL,
+  `ville_client` varchar(255) NOT NULL,
+  `code_postale_client` int(11) NOT NULL,
+  `adresse_client` varchar(255) NOT NULL,
+  `email_client` text NOT NULL,
+  `created_at_client` timestamp NOT NULL,
+  PRIMARY KEY (`id_client`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id_commande` int(11) NOT NULL AUTO_INCREMENT,
+  `id_client` int(11) NOT NULL,
+  `date_commande` timestamp NOT NULL,
+  `motant_commande` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`id_commande`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte_users`
+--
+
+DROP TABLE IF EXISTS `compte_users`;
+CREATE TABLE IF NOT EXISTS `compte_users` (
+  `id_compte` int(11) NOT NULL AUTO_INCREMENT,
+  `client_id` int(11) NOT NULL,
+  `login_user` varchar(180) NOT NULL,
+  `password_user` varchar(255) NOT NULL,
+  `type_compte` varchar(200) NOT NULL,
+  `user_avatar` varchar(300) NOT NULL,
+  PRIMARY KEY (`id_compte`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `detail_commande`
+--
+
+DROP TABLE IF EXISTS `detail_commande`;
+CREATE TABLE IF NOT EXISTS `detail_commande` (
+  `id_detail_commande` int(11) NOT NULL AUTO_INCREMENT,
+  `id_commande` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL,
+  `quantite_produit` int(11) NOT NULL,
+  PRIMARY KEY (`id_detail_commande`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE IF NOT EXISTS `product` (
+  `id_product` int(11) NOT NULL AUTO_INCREMENT,
+  `name_product` int(11) NOT NULL,
+  `description_product` int(11) NOT NULL,
+  `price_product` decimal(10,0) NOT NULL,
+  `quantite_product` int(11) NOT NULL,
+  `id_categories` int(11) NOT NULL,
+  PRIMARY KEY (`id_product`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `subcategories`
+--
+
+DROP TABLE IF EXISTS `subcategories`;
+CREATE TABLE IF NOT EXISTS `subcategories` (
+  `id_subcategories` int(11) NOT NULL AUTO_INCREMENT,
+  `name_subcategories` varchar(255) NOT NULL,
+  `categories_id` int(11) NOT NULL,
+  PRIMARY KEY (`id_subcategories`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/import/header.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+
+<nav>
+    <ul>
+        <li>
+            <a href="index.php">Home</a>
+        </li>
+        <li>
+            <a href="boutique.php">Boutiques</a>
+        </li>
+        <li>
+            <form action="" method="post">
+                <input type="text" name="search-bar" placeholder="Search">
+            </form>
+        </li>
+    </ul>
+</nav>
