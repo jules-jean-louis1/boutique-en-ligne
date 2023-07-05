2afecf296473f199f99a92a6f23d8f9f21320085
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-07-05 12:00:05 +0200

    fix commande probleme index

--- src/php/Classes/Order.php
+++ src/php/Classes/Order.php
@@ -81,11 +81,10 @@ class Order extends Database
     public function numberItemsInOrder($id)
     {
         $bdd = $this->getBdd();
-        $req = $bdd->prepare("SELECT SUM(quantite_produit) AS total_articles
-                                    FROM commande c
-                                    JOIN detail_commande d ON c.id_commande = d.command_id
-                                    WHERE c.users_id = :id
-                                    GROUP BY c.id_commande");
+        $req = $bdd->prepare("SELECT SUM(quantite_produit) AS total_articles 
+                                    FROM commande c 
+                                    JOIN detail_commande d ON c.id_commande = d.command_id 
+                                    WHERE d.command_id = :id");
         $req->execute(array(
             "id" => $id
         ));
@@ -180,5 +179,41 @@ class Order extends Database
         $req->execute(array(
             "id_commande" => $id_commande
         ));
+    }
+    public function getAllOrder(int $ID)
+    {
+        $bdd = $this->getBdd();
+        $req1 = $bdd->prepare("SELECT id_commande, date_commande, motant_commande, statue_commande, users_id FROM commande WHERE users_id = :ID");
+        $req1->execute(array(
+            "ID" => $ID
+        ));
+        $result1 = $req1->fetchAll(PDO::FETCH_ASSOC);
+        return $result1;
+    }
+    public function getDetailOrder(int $id_commande)
+    {
+        $bdd = $this->getBdd();
+        $req = $bdd->prepare("SELECT p.img_product, p.name_product, p.price_product, dc.quantite_produit, s.name_subcategories 
+                                    FROM detail_commande dc 
+                                    JOIN product p ON p.id_product = dc.product_id 
+                                    JOIN subcategories s ON s.id_subcategories = p.subcategories_id 
+                                    JOIN commande c ON c.id_commande = dc.command_id 
+                                    JOIN client cl ON cl.id_client = c.users_id 
+                                    WHERE c.id_commande = :id_commande");
+        $req->execute(array(
+            "id_commande" => $id_commande
+        ));
+        $result = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+    public function getCommandeByID(int $id_commande)
+    {
+        $bdd = $this->getBdd();
+        $req = $bdd->prepare("SELECT id_commande, date_commande, motant_commande, statue_commande, users_id FROM commande WHERE id_commande = :id_commande");
+        $req->execute(array(
+            "id_commande" => $id_commande
+        ));
+        $result = $req->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
     }
 }
--- src/php/fetch/profil/commandeClient.php
+++ src/php/fetch/profil/commandeClient.php
@@ -1,19 +1,30 @@
 <?php
 session_start();
-require_once "../../Classes/Product.php";
 require_once "../../Classes/Order.php";
 
 if(isset($_SESSION['id'])) {
     $id = $_SESSION['id'];
     $order = new Order();
     $orderUser = $order->getCommandeByUserId($id);
-    if(count($orderUser) > 0) {
-        $orderDetails = $order->getDetailCommandeById($id);
-        $totalItems = $order->numberItemsInOrder($id);
+    if (count($orderUser) > 0) {
+        $response = [];
+        $getAllOrder = $order->getAllOrder($id);
+        foreach ($getAllOrder as $item) {
+            $orderDetails = $order->getDetailOrder($item['id_commande']);
+            $totalItems = $order->numberItemsInOrder($item['id_commande']);
+            $orderUser = $order->getCommandeByID($item['id_commande']);
+
+            $response[] = [
+                "Commande" => $orderUser,
+                "DetailCommande" => $orderDetails,
+                "totalItems" => $totalItems
+            ];
+        }
         header("Content-Type: application/json");
-        echo json_encode(array("status" => "success", "Commande" => $orderUser, "DetailCommande" => $orderDetails, "totalItems" => $totalItems));
+        echo json_encode(["status" => "success", "order" => $response]);
     } else {
         header("Content-Type: application/json");
-        echo json_encode(array("status" => "error", "message" => "Aucune commande trouvée"));
+        echo json_encode(["status" => "error", "message" => "Aucune commande trouvée"]);
     }
+
 }
