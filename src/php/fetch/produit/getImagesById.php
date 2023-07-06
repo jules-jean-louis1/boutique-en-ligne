dc11e30f892a40c331e6091ebc953be21fb2a359
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-07-06 11:42:33 +0200

    affichage depuis le serveur plutôt que depuis js

--- src/php/fetch/produit/getImagesById.php
+++ src/php/fetch/produit/getImagesById.php
@@ -6,6 +6,4 @@ if (isset($_GET['id'])) {
     $id = intval(htmlspecialchars($_GET['id']));
     $product = new Product();
     $images = $product->getProductImageById($id);
-    /*header("Content-Type: application/json");
-    echo json_encode(["status" => "success", "images" => $images]);*/
 }
